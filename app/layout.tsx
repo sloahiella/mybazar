d3a01461a9aa853bf0c3ba8f52b431503587e9ba
Sohel <sloahiella@gmail.com>
2026-08-31 10:33:01 +0600

    Add PWA manifest.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+// @ts-ignore
+import "./globals.css";
 import "./globals.css";
 import Script from "next/script";
 // @ts-ignore
